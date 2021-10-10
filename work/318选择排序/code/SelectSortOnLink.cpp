dd4b9b601e4d8ae2da356a18430a7d5c2004e009
jz19190248 <2129056867@qq.com>
2021-10-10 22:27:49 +0800

    Add selectSort works

---
+++ work/318选择排序/code/SelectSortOnLink.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define ElemType int
+using namespace std;
+typedef struct
+{
+    Node *pointer;
+    ElemType value;
+} Node;
+
+void SelcetSort(Node *link)
+{
+    Node *p = link->pointer;
+    Node *h = link, *p, *q, *r, *s;
+    while (h != NULL)
+    {
+    }
+}
