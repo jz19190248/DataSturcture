7e3bf2d6f91ba260b6ad72d5d075fdc1b5289f60
jz19190248 <2129056867@qq.com>
2021-10-11 21:44:59 +0800

    Finish SelectSort

--- work/318选择排序/code/SelectSortOnLink.cpp
+++ work/318选择排序/code/SelectSortOnLink.cpp
@@ -11,7 +11,29 @@ void SelcetSort(Node *link)
 {
     Node *p = link->pointer;
     Node *h = link, *p, *q, *r, *s;
+    link = NULL;
     while (h != NULL)
     {
+        p = s = h;
+        //p为工作指针，s为标记指针，q为p的前驱指针，r为s的前驱指针
+        q = r = NULL;
+        while (p != NULL)
+        {
+            if (p->value > s->value)
+            {
+                s = p;
+                r = q;
+            }
+            q = p;
+            p = p->pointer;
+        }
+        if (s == h)
+        {
+            h = h->pointer;
+        }
+        else
+            r->pointer = s->pointer;
+        s->pointer = link;
+        link = s;
     }
 }
